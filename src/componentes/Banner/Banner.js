e64c10923168b21d1cd7cbe020320891eeaa5ec4
Roberta Santana <g.roberta.s@hotmail.com>
2024-07-10 19:45:14 -0300

    Comp Banner finalizado

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+
+function Banner(){
+    //JSX(Como o react trabalha com a parte visual)
+    return(
+        <header className='banner'>
+        <img src="/imagens/banner.png" alt="Banner principal"></img>
+        </header>
+
+    )
+}
+
+export default Banner;
