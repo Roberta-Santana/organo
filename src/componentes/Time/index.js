bea007cc8c210aa7e94e9c32a46395a7d3c14435
Roberta Santana <g.roberta.s@hotmail.com>
2024-07-16 17:49:19 -0300

    ajuste

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -10,10 +10,11 @@ const Time = (props)=>{
 
         //renderização condicional:
         (props.colaboradores.length>0) && <section className="time" style={css}>
+            <h1>Minha Organização</h1>
             <h3 style={css}>{props.nome}</h3>
             
             <div className='colaboradores'>
-                {props.colaboradores.map(colaborador=> 
+                {props.colaboradores.map((colaborador)=> 
                     <Colaborador corDeFundo={props.corPrimaria}
                     key={colaborador.nome}
                     nome={colaborador.nome}
